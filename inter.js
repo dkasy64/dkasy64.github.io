a0231612b0ebc06b73c10055faf9467345883a72
dkasy64 <dawitkasy64@gmail.com>
2025-10-01 18:12:38 -0400

    updates

---
+++ inter.js
@@ -0,0 +1,13 @@
+console.log('Hi mom');
+
+let luskyNum; //declaring variable
+
+const name = 'blue' // same as static/ cannot be reassigned
+
+function fun(){
+    let a = 's'
+}
+
+if(true){
+    let a = 'block'
+}
